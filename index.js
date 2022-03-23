1a18b9b3156966fe01edc2dfb7f4b7a682118b21
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-03-23 00:23:15 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+    let messages = [];
+    for (let index = 0; index < names.length; index++) {
+        messages[index] = 'Thank you, ' + names[index] + ', for the wonderful ' + eventName + ' gift!';
+    }
+
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
